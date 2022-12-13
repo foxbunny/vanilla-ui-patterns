0e36106a8ac6df04bb2697539954d45bcdaba0ed
Hajime Yamasaki Vukelic <hi@devi.us>
2022-12-13 01:19:20 +0100

    Fix missing .matches and parentheses

--- dark-mode/client.js
+++ dark-mode/client.js
@@ -19,7 +19,7 @@
     // https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-color-scheme
     // and the Rendering tab in chrome dev tools (look at more tools menu if not shown)
     restoreDarkMode = () => document.body.classList.toggle('dark', localStorage.darkMode == null
-      ? matchMedia?.('prefers-color-scheme: dark')
+      ? matchMedia?.('(prefers-color-scheme: dark)').matches
       : localStorage.darkMode === 'true'),
     enableAnimation = () => document.body.setAttribute('data-animation-enabled', 'true'),
     disableAnimation = () => document.body.removeAttribute('data-animation-enabled')
